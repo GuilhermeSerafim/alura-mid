5212df10017b76abac02ebe092fbabe820b236ec
GuilhermeSerafim <guilerstudies@gmail.com>
2023-11-29 13:10:29 -0300

    Adicionando a classe 'ativa' para o elemento que for pressionado com 'enter' ou 'espaço'

--- main.js
+++ main.js
@@ -2,10 +2,19 @@ const listaDeTeclas = document.querySelectorAll('input[type=button]');
 const inputTel = document.querySelector('input[type=tel]');
 
 for (let indice = 0; indice < listaDeTeclas.length; indice++) {
-  
-  const tecla = listaDeTeclas[indice];
-  
-  tecla.onclick = function () {
-    inputTel.value = inputTel.value + tecla.value;
-  }
+
+    const tecla = listaDeTeclas[indice];
+
+    tecla.onclick = function () {
+        inputTel.value = inputTel.value + tecla.value;
+    }
+
+    tecla.onkeydown = (evento) => {
+        if (evento.code === "Enter" || evento.code === "Space") {
+            tecla.classList.add('ativa');
+        }
+    }
+    tecla.onkeyup = () => {
+        tecla.classList.remove('ativa');
+    }
 }
