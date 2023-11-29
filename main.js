8605b8995441f4288dc2aba00faea65e4073a285
GuilhermeSerafim <guilerstudies@gmail.com>
2023-11-29 10:54:16 -0300

    Deixando código funcional com template String

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
-function tocaSom(idElementAudio) {
+function tocaSom(idElementAudio) { //Está preparado para receber um id dinamicamente
     document.querySelector(idElementAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 let contador = 0;
-//enquanto
-while (contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = function () { //Quando criamos uma função apenas declaramos que ela existe, e nao executa 
-        tocaSom('#som_tecla_pom')
+
+while (contador < listaDeTeclas.length) { //Percorre o array
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1]; //Acessando a segunda classe
+    console.log(instrumento);
+
+    const idAudio = `#som_${instrumento}`; //Template String
+    console.log(idAudio);
+
+    tecla.onclick = function () { //Quando criamos uma função apenas declaramos que ela existe, e nao executa 
+        tocaSom(idAudio);
     }
     console.log(contador);
     contador++;
-}
+} 
